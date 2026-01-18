478debc07d0e71e62fb7b50e94a997b106d26f02
Anastasiya <aiskoskina@yandex.ru>
2026-01-18 15:19:33 +0300

    update get userID from cookie

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -337,7 +337,7 @@ func DBHealthCheck(response http.ResponseWriter, request *http.Request, storage 
 }
 
 func UserURLSHandler(response http.ResponseWriter, request *http.Request, baseURL string, storage repository.URLStorage) {
-    userID := request.Context().Value("user_id")
+	userID := request.Context().Value(UserIDKey)
     if userID == nil {
         response.Header().Set("Content-Type", "application/json")
         response.WriteHeader(http.StatusUnauthorized)
@@ -392,7 +392,6 @@ func UserURLSHandler(response http.ResponseWriter, request *http.Request, baseUR
 func DeleteUserURLSHandler(response http.ResponseWriter, request *http.Request, baseURL string, storage repository.URLStorage) {
 	logger.Log.Info("DeleteUserURLSHandler called")
 	userID := request.Context().Value(UserIDKey)
-	fmt.Println(userID)
 	if userID == nil {
 		response.Header().Set("Content-Type", "application/json")
 		response.WriteHeader(http.StatusUnauthorized)
